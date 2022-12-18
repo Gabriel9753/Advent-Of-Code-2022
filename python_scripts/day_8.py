36e488afb89b799a778e00577e093673c6d3d7c8
Gabriel9753 <scga1011@h-ka.de>
2022-12-18 16:54:01 +0100

    Update day 8 with optimized solution

--- python_scripts/day_8.py
+++ python_scripts/day_8.py
@@ -4,25 +4,25 @@ from loguru import logger
 from aocd import get_data
 from collections import defaultdict
 import math
-logger.remove()
+# logger.remove()
 logger.add("logging.log", level="INFO")
 day = sys.argv[0].split("/")[-1].split("_")[1].removesuffix(".py")
 
+VISIBLES = set()
 def read_file(file_path):
     with open(file_path, encoding="utf-8") as f:
         return f.read()
 
-def search_part_1(grid, visibles, iter_x, iter_y, vertical=True):
-    visible = []
+def search_part_1(grid, iter_x, iter_y, vertical=True):
+    global VISIBLES
     for x in iter_x:
         current_tallest = -1
         for y in iter_y:
             _x, _y = (y, x) if vertical else (x, y)
             if (tree_height := grid[_x][_y]) > current_tallest:
                 current_tallest = tree_height
-                visible.append((_y, _x))
+                VISIBLES.add((_y, _x))
             if tree_height >= 9: break
-    return visibles + visible
 
 def calc_score_from_point(grid, grid_x, grid_y, x, y):
     directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
@@ -54,19 +54,23 @@ def search_part_2(grid, grid_x, grid_y):
             score = calc_score_from_point(grid, grid_x, grid_y, x, y)
             scores[(x, y)] = score
     return max(scores.values())
-            
+
 def solve(puzzle_input):
+    global VISIBLES
     logger.info(f"Solving puzzle - day {day}...")
     grid = [[int(i) for i in line] for line in puzzle_input.splitlines()]
     grid_x = len(grid[0])
     grid_y = len(grid)
-    visibles = []
-    visibles = search_part_1(grid, visibles, range(grid_x), range(grid_y))
-    visibles = search_part_1(grid, visibles, range(grid_x), range(grid_y-1, -1, -1))
-    visibles = search_part_1(grid, visibles, range(grid_y), range(grid_x), vertical=False)
-    visibles = search_part_1(grid, visibles, range(grid_y), range(grid_x-1, -1, -1), vertical=False)
-    logger.info(f"Solution Part 1: {len(set(visibles))}")
-    logger.info(f"Solution Part 2: {search_part_2(grid, grid_x, grid_y)}")            
+    start = time()
+    search_part_1(grid, range(grid_x), range(grid_y))
+    search_part_1(grid, range(grid_x), range(grid_y-1, -1, -1))
+    search_part_1(grid, range(grid_y), range(grid_x), vertical=False)
+    search_part_1(grid, range(grid_y), range(grid_x-1, -1, -1), vertical=False)
+    logger.info(f"Solution Part 1: {len(VISIBLES)}")
+    print(f"Time taken: {time() - start:.15f}s")
+    start = time()
+    logger.info(f"Solution Part 2: {search_part_2(grid, grid_x, grid_y)}")     
+    print(f"Time taken: {time() - start:.15f}s")       
 
 if __name__ == '__main__':
     # if getting data from aoc via api
