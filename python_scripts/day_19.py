80928ad09f64a451081c61c64c5cf12a4e9e56e1
Gabriel9753 <scga1011@h-ka.de>
2022-12-21 21:05:29 +0100

    Add solution for day 19 and 21

--- python_scripts/day_19.py
+++ python_scripts/day_19.py
@@ -1,7 +1,10 @@
+import re
 import sys
 from time import time
 from loguru import logger
 from aocd import get_data
+from collections import deque
+from tqdm import tqdm
 # logger.remove()
 logger.add("logging.log", level="INFO")
 day = sys.argv[0].split("/")[-1].split("_")[1].removesuffix(".py")
@@ -10,11 +13,97 @@ def read_file(file_path):
     with open(file_path, encoding="utf-8") as f:
         return f.read()
 
+blueprints = {}
+
+def max_robots_needed(blueprint):
+    # 0 ore robots, 1 clay robots, 2 obsidian robots
+    _max = {}
+    max_ore_robots = max(blueprint["ore_robot"], blueprint["clay_robot"], blueprint["obsidian_robot"][0], blueprint["geode_robot"][0])
+    max_clay_robots = blueprint["obsidian_robot"][1]
+    max_obsidian_robots = blueprint["geode_robot"][1]
+    _max["ore_robots"] = max_ore_robots
+    _max["clay_robots"] = max_clay_robots
+    _max["obsidian_robots"] = max_obsidian_robots
+    return _max
+    
+def search_max_geodes(blueprint_id, max_time):
+    blueprint = blueprints[blueprint_id]
+    max_robots = max_robots_needed(blueprint)
+    _max = 0
+    # 0 ore robot, 1 clay robot, 2 obsidian robot, 3 geode robot
+    # 4 current ore, 5 current clay, 6 current obsidian, 7 current geode
+    # 8 current time stamp
+    # 9 built ore robots, 10 built clay robots, 11 built obsidian robots, 12 built geode robots
+    start = (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    max_time = max_time
+    queue = deque([start])
+    visited = set()
+    while queue:
+        popped_item = queue.pop()
+        ore_robots, clay_robots, obsidian_robots, geode_robots, ores, clays, obsidians, geodes, _time, built_ore, built_clay, built_obs, built_geode = popped_item
+        if _time == max_time:
+            _max = max(_max, geodes)
+            continue
+        delta_time = max_time-_time
+        possible_geode_robots = int(delta_time/2)
+        if tuple(popped_item) in visited:
+            continue
+        visited.add(tuple(popped_item))
+        if geodes + delta_time * (geode_robots + possible_geode_robots) < _max:
+            continue
+        ore_robot_costs, clay_robot_costs, obsidian_robot_costs, geode_robot_costs = blueprint["ore_robot"], blueprint["clay_robot"], blueprint["obsidian_robot"], blueprint["geode_robot"]
+        
+        _ores = ores + ore_robots
+        _clays = clays + clay_robots
+        _obsidians = obsidians + obsidian_robots
+        _geodes = geodes + geode_robots
+        
+        if ore_robots <= max_robots["ore_robots"] and _ores >= ore_robot_costs:
+            queue.append((ore_robots+built_ore, clay_robots+built_clay, obsidian_robots+built_obs, geode_robots+built_geode, _ores-ore_robot_costs, _clays, _obsidians, _geodes, _time+1, 1, 0, 0, 0))
+        if clay_robots <= max_robots["clay_robots"] and _ores >= clay_robot_costs and _clays < obsidian_robot_costs[1]:
+            queue.append((ore_robots+built_ore, clay_robots+built_clay, obsidian_robots+built_obs, geode_robots+built_geode, _ores-clay_robot_costs, _clays, _obsidians, _geodes, _time+1, 0, 1, 0, 0))
+        if obsidian_robots <= max_robots["obsidian_robots"] and (_ores >= obsidian_robot_costs[0] and _clays >= obsidian_robot_costs[1]):
+            queue.append((ore_robots+built_ore, clay_robots+built_clay, obsidian_robots+built_obs, geode_robots+built_geode, _ores-obsidian_robot_costs[0], _clays-obsidian_robot_costs[1], _obsidians, _geodes, _time+1, 0, 0, 1, 0))
+        if _ores >= geode_robot_costs[0] and _obsidians >= geode_robot_costs[1]:
+            queue.append((ore_robots+built_ore, clay_robots+built_clay, obsidian_robots+built_obs, geode_robots+built_geode, _ores-geode_robot_costs[0], _clays, _obsidians-geode_robot_costs[1], _geodes, _time+1, 0, 0, 0, 1))
+        
+        queue.append((ore_robots+built_ore, clay_robots+built_clay, obsidian_robots+built_obs, geode_robots+built_geode, _ores, _clays, _obsidians, _geodes, _time+1, 0, 0, 0, 0))
+            
+    return _max
+
 def solve(puzzle_input):
     logger.info(f"Solving puzzle - day {day}...")
-    logger.info("puzzle_input:", puzzle_input)
+    blueprints_raw = puzzle_input.strip().split('Blueprint')
+    for i, blueprint_raw in enumerate(blueprints_raw[1:], start=1):
+        blueprint = {}
+        ore_costs = re.findall(r'.* (\d+) ore.*', blueprint_raw)
+        blueprint["ore_robot"] = (int(ore_costs[0]))
+        blueprint["clay_robot"] = (int(ore_costs[1]))
+        clay_costs = re.findall(r'.* (\d+) clay.*', blueprint_raw)[0]
+        blueprint["obsidian_robot"] = (int(ore_costs[2]), int(clay_costs))
+        obsidian_costs = re.findall(r'.* (\d+) obsidian.*', blueprint_raw)[0]
+        blueprint["geode_robot"] = (int(ore_costs[3]), int(obsidian_costs))
+        blueprints[i] = blueprint
+    print(blueprints)
+    
+    # for k in tqdm(blueprints.keys()):
+    #     max_geodes = search_max_geodes(k, 24)
+    #     blueprints[k]["max_geodes"] = k * max_geodes
+    # result_1 = sum([blueprint["max_geodes"] for blueprint in blueprints.values()])
+    # print(f"Result 1: {result_1}")
+    
+    for k in tqdm(blueprints.keys()):
+        max_geodes = search_max_geodes(k, 32)
+        blueprints[k]["max_geodes"] = max_geodes
+    result_2 = [blueprint["max_geodes"] for blueprint in blueprints.values()]
+    r_2 = 1
+    for r in result_2:
+        r_2 *= r
+        print(r)
+    print(f"Result 2: {r_2}")
+    
 
 if __name__ == '__main__':
     # if getting data from aoc via api
     # solve(get_data(year=2022, day=int(day)))
-    solve(read_file(f"python_scripts/day_{day}.txt"))
+    solve(read_file(f"python_scripts/day_{day}_2.txt"))
--- python_scripts/day_21.py
+++ python_scripts/day_21.py
@@ -2,6 +2,8 @@ import sys
 from time import time
 from loguru import logger
 from aocd import get_data
+import sympy
+from time import time
 # logger.remove()
 logger.add("logging.log", level="INFO")
 day = sys.argv[0].split("/")[-1].split("_")[1].removesuffix(".py")
@@ -10,9 +12,66 @@ def read_file(file_path):
     with open(file_path, encoding="utf-8") as f:
         return f.read()
 
+monkeys = {}
+
+def calc_for_monkey(monkey):
+    job = monkeys[monkey]
+    if isinstance(job, int):
+        return job
+    elif monkey == "root":
+        splitted_job = job.split(" ")
+        monkey_1 = splitted_job[0]
+        monkey_2 = splitted_job[-1]
+        _operator = splitted_job[1]
+        return eval(f"{calc_for_monkey(monkey_1)} {_operator} {calc_for_monkey(monkey_2)}")
+    else:
+        splitted_job = job.split(" ")
+        monkey_1 = splitted_job[0]
+        monkey_2 = splitted_job[-1]
+        _operator = splitted_job[1]
+        return f"({calc_for_monkey(monkey_1)} {_operator} {calc_for_monkey(monkey_2)})"
+
+def part_2(monkey):
+    job = monkeys[monkey]
+    if monkey == "humn":
+        return "X"
+    elif isinstance(job, int):
+        return f"{job}"
+    else:
+        splitted_job = job.split(" ")
+        monkey_1 = splitted_job[0]
+        monkey_2 = splitted_job[-1]
+        _operator = splitted_job[1]
+        if monkey == "root":
+            return f"{part_2(monkey_1)} = {part_2(monkey_2)}"
+        else:
+            return f"({part_2(monkey_1)} {_operator} {part_2(monkey_2)})"
+
 def solve(puzzle_input):
     logger.info(f"Solving puzzle - day {day}...")
     logger.info("puzzle_input:", puzzle_input)
+    for line in puzzle_input.splitlines():
+        monkey = line.split(":")[0]
+        _value = line.split(":")[1].strip()
+        try:
+            _value = int(_value)
+        except ValueError:
+            pass
+        monkeys[monkey] = _value
+    
+    start = time()
+    print(f"Part 1: {int(calc_for_monkey('root'))}")
+    print(f"Time: {time() - start}")
+    start = time()
+    X = sympy.symbols('X')
+    eq_string = part_2("root")
+    equations = eq_string.split("=")
+    eq_1, eq_2 = equations[0].strip(), equations[1].strip()
+    equation = sympy.Eq(sympy.sympify(eq_1), sympy.sympify(eq_2))
+    # Löse die Gleichung nach X
+    solution = sympy.solve(equation, X)
+    print(f"Part 2: {solution[0]}")
+    print(f"Time: {time() - start}")
 
 if __name__ == '__main__':
     # if getting data from aoc via api
