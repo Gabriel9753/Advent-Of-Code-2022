599dc3f63e92eacf0c8ab8768a5ba67462260203
Gabriel9753 <scga1011@h-ka.de>
2022-12-25 14:15:44 +0100

    Update day 24 for efficient simulation of blizzard

--- python_scripts/day_24.py
+++ python_scripts/day_24.py
@@ -7,7 +7,7 @@ def read_file(file_path):
     with open(file_path, encoding="utf-8") as f:
         return f.read()
 
-_map = {}
+_map = set()
 _cache = {}
 
 blizzard_types = {
@@ -56,39 +56,42 @@ def score_position(start_pos, end_pos, _time):
 
 def dfs(start_pos, end_pos, start_time=0):
     move_directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)]
-    queue = deque([(start_pos, start_time, [])])
+    queue = deque([(start_pos, start_time)])
     visited = set()
     best_time = math.inf
     time_depth = start_time
     while queue:
-        current_pos, _time, bm = queue.popleft()
-        
+        current_pos, _time = queue.popleft()
+            
         if _time > time_depth:
             time_depth = _time
             queue = deque(sorted(queue, key=lambda x: score_position(x[0], end_pos, x[1]), reverse=True))
-            queue = deque(list(queue)[:300])
+            queue = deque(list(queue)[:250])
             
         if current_pos == end_pos:
-            if _time < best_time:
-                best_moves = bm
-                best_time = _time
-            continue
+            best_time = min(best_time, _time)
+            break
+        
         if (current_pos, _time) in visited: continue
+        
         visited.add((current_pos, _time))
+        
         if not is_optimizable(current_pos, end_pos, _time, best_time): continue
-        current_blizzards = _cache.get(_time+1)
+        
+        blizzard_time = (_time+1) % (Blizzard.width*Blizzard.height)
+        current_blizzards = _cache.get(blizzard_time)
         if current_blizzards is None:
-            current_blizzards = Blizzard.get_blizzards(_time+1)
-            _cache[_time+1] = current_blizzards
+            current_blizzards = Blizzard.get_blizzards(blizzard_time)
+            _cache[blizzard_time] = current_blizzards
             
         for move in move_directions:
             new_pos = (current_pos[0] + move[0], current_pos[1] + move[1])
             if new_pos in current_blizzards:
                 continue
-            if _map.get(new_pos) == "#" or _map.get(new_pos) is None:
-                continue
-            queue.append((new_pos, _time+1, bm+[(current_pos, new_pos, _time)]))
-    return best_time, best_moves
+            if new_pos in _map:
+                queue.append((new_pos, _time+1))
+                
+    return best_time
 
 
 def solve(puzzle_input):
@@ -96,7 +99,10 @@ def solve(puzzle_input):
     blizzards = []
     for row, line in enumerate(puzzle_input.splitlines()):
         for col, char in enumerate(line):
-            _map[(col, row)] = "#" if char == "#" else "."
+            # _map[(col, row)] = "#" if char == "#" else "."
+            if char != "#":
+                _map.add((col, row))
+            
             if char in blizzard_types.keys():
                 blizz = Blizzard((col, row), blizzard_types[char])
                 blizzards.append(blizz)
@@ -108,17 +114,16 @@ def solve(puzzle_input):
     
     # Part 1
     start_1_time = time.time()
-    best_time, best_moves = dfs(start_pos, end_pos, start_time=0)
+    best_time = dfs(start_pos, end_pos, start_time=0)
     print(f"Part 1: {best_time} in {time.time() - start_1_time:.4f} seconds!")
     
     # Part 2
     start_2_time = time.time()
-    best_moves_2 = best_moves
-    bm = best_moves
     best_time_2 = best_time
+    start_pos, end_pos = end_pos, start_pos
     for _ in range(2):
-        best_time_2, bm = dfs(bm[-1][1], bm[0][0], start_time=best_time_2)
-        best_moves_2.extend(bm)
+        best_time_2 = dfs(start_pos, end_pos, start_time=best_time_2)
+        start_pos, end_pos = end_pos, start_pos
         
     print(f"Part 2: {best_time_2} in {time.time() - start_2_time:.4f} seconds!")
     print(f"Total time: {time.time() - start_full_time:.4f} seconds!")
